1e46ea2376c41b28058f1ee750b73b68c1031fce
Michael McCullough <michael.mccullough21@gmail.com>
2019-08-20 18:46:55 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,24 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+      VALUES ("Mr. Chocolate", 12, "male", "brown", "mean", 1)
+end
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+      VALUES ("Rowdy", 11, "male", "black", "nice", 0)
+end
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+      VALUES ("Tabitha", 9, "female", "brown", "nice", 1)
+end
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+      VALUES ("Sergeant Brown",
+end
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+      VALUES ("Melissa", 
+end
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+      VALUES ("Grinch", 
+end
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+      VALUES ("Wendy", 
+end
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+      VALUES (null, 
+end
